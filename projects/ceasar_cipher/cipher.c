a21b61b393b6a08406b99cc33a958832faaba73a
Khornne <maurice.pattinsonn@gmail.com>
2025-05-23 14:07:13 +0100

    Created character validation

--- projects/ceasar_cipher/cipher.c
+++ projects/ceasar_cipher/cipher.c
@@ -10,7 +10,32 @@ int main () {
   scanf("%s", cipher);
 
   printf("Enter key: ");
-  scanf("%d", key);
+  scanf("%d", &key);
 
+
+  for (int i = 0; cipher[i] != '\0'; ++i) {
+
+  ch = cipher[i];
+
+  if (isalnum(ch)) {
+
+    if (islower(ch)){
+      ch = (ch - 'a' + key) % 26 + 'a';
+    }
+    if (isupper(ch)){
+      ch = (ch - 'A' + key) % 26 + 'A';
+    }
+    if (isdigit(ch)) {
+      ch = (ch - '0' + key) % 26 + '0';
+    }
+
+  else {
+    printf("Invalid Message\n");
 }
+      
+    printf("Encrypted message: %s\n", cipher);
+    return 0;
+  }  
 
+  }
+}
